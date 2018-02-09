1d4f38b5d8304948b17b9cfee447d21ffafdcfe7
noname258 <36302555+noname258@users.noreply.github.com>
2018-02-09 22:42:48 +0800

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,23 @@
+javascript:
+(
+	function(j,f)
+	{
+		j=['https://noname258.github.io/2516/data.js',
+		   'https://noname258.github.io/2516/calc.js',
+		   'https://noname258.github.io/2516/body.js'];
+		f=function(s,u)
+		{
+			if(j.length==0)
+			{
+				return;
+			}
+			u=j.shift()+'?'+Date.now();
+			s=document.createElement('script');
+			document.body.appendChild(s);
+			s.charset='UTF-8';
+			s.addEventListener('load',f);
+			s.src=u;
+		};
+		(document.readyState=='loading')?document.addEventListener('DOMContentLoaded',f):f();
+	}
+)();
