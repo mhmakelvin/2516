599fe200cacda788afcaa5b5b7cb5870a11b98cf
noname258 <36302555+noname258@users.noreply.github.com>
2018-03-08 15:58:36 +0800

    Update testbody.js

--- testbody.js
+++ testbody.js
@@ -447,7 +447,7 @@ function print_result(golliramode, homeaddr)
 	
 			tmplv=(datalist[i].lv[2].slice(-1)=='-')?(datalist[i].lv[2].slice(0, -1)):
 				(datalist[i].lv[2].slice(-1)=='=')?(datalist[i].lv[2].slice(0, -1)):datalist[i].lv[2];
-			result_str += "<th class=mai_remaster>" + tmplv + "<\/th>";
+			result_str += "<th class=mai_remaster>" + "mra_diff2tmp(tmplv)" + "<\/th>";
 			result_str += "<th class=mai_remaster>" + (100*datalist[i].achive[2]).toFixed(4) + "%<\/th>";
 			result_str += "<\/tr>";
 			
@@ -461,7 +461,7 @@ function print_result(golliramode, homeaddr)
 		tmplv=(datalist[i].lv[1].slice(-1)=='-')?(datalist[i].lv[1].slice(0, -1)):
 			(datalist[i].lv[1].slice(-1)=='=')?(datalist[i].lv[1].slice(0, -1)):datalist[i].lv[1];
 		
-		result_str += "<th class=mai_master>" + "10" + "<\/th>";
+		result_str += "<th class=mai_master>" + "mra_diff2tmp(tmplv)" + "<\/th>";
 		result_str += "<th class=mai_master>" + (100*datalist[i].achive[1]).toFixed(4) + "%<\/th>";
 		result_str += "<\/tr>";
 
