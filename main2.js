aac9ff55451f8d8aa9904d378da413b7ef61fb64
noname258 <36302555+noname258@users.noreply.github.com>
2018-03-23 00:22:45 +0800

    Update main2.js

--- main2.js
+++ main2.js
@@ -2,7 +2,7 @@ javascript:
 (
 	function(j,f)
 	{
-		j=['https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level_milk_lock.js',
+		j=['https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level_milk2.js',
 		   'https://noname258.github.io/2516/calc.js',
 		   'https://noname258.github.io/2516/testbody.js'];
 		f=function(s,u)
